5702fa4912ac303f1eba09afc9638d096786c8e6
Jesse Stippel <SkyeStippel@Gmail.com>
2015-11-12 17:03:00 +0100

    twitter module upgraded

--- src/operun/tweeter/browser/tweeter.py
+++ src/operun/tweeter/browser/tweeter.py
@@ -25,7 +25,7 @@ class TweeterView(BrowserView):
     def tweet_count(self):
         #Get number of tweets from formula and return as integer.
         form = self.request.form
-        this_result = int(form.get('tweet_count', '5'))
+        this_result = int(form.get('tweet_count', '6'))
 
         return this_result
 
@@ -34,12 +34,35 @@ class TweeterView(BrowserView):
         name = self.twitter_user()
 
         if name:
-            all_tweets = t.statuses.user_timeline(screen_name='%s' % (name))
-            tweet_list = all_tweets[:self.tweet_count()]
+            if '#' in name:
+                all_tweets = t.search.tweets(q="%s" % (name))
+                status_tweets = all_tweets['statuses']
+                tweet_list = status_tweets[:self.tweet_count()]
 
-            return all_tweets[:self.tweet_count()]
+                return tweet_list
+            if '@' in name:
+                all_tweets = t.statuses.user_timeline(screen_name='%s' % (name))
+                tweet_list = all_tweets[:self.tweet_count()]
+
+                return tweet_list
+            else:
+                all_tweets = t.statuses.user_timeline(screen_name='%s' % (name))
+                tweet_list = all_tweets[:self.tweet_count()]
+
+                return tweet_list
         else:
             return []
+
+    def replace_url_to_link(self, value):
+        #Replace url to link.
+        urls = re.compile(r"((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)", re.MULTILINE|re.UNICODE)
+        value = urls.sub(r'<a href="\1">\1</a>', value)
+
+        #Replace email to mailto.
+        urls = re.compile(r"([\w\-\.]+@(\w[\w\-]+\.)+[\w\-]+)", re.MULTILINE|re.UNICODE)
+        value = urls.sub(r'<a href="mailto:\1">\1</a>', value)
+
+        return value
 
     def tweet_data(self):
         #Useable info from tweet data.
@@ -48,6 +71,7 @@ class TweeterView(BrowserView):
         for tweet in self.twitter_tweets():
             user = tweet['user']
 
+            #Error handling.
             if 'profile_banner_url' in user:
                 profile_banner_url = user['profile_banner_url']
             else:
@@ -58,18 +82,21 @@ class TweeterView(BrowserView):
             else:
                 user_location = ''
 
+            #Variables to pass through page template.
             profile_bigger = user['profile_image_url'].replace('_normal', '')
             profile_color = 'background-color:' + ' #' + user['profile_link_color'] + ';'
+            profile_border_color = 'border: 1px solid #' + user['profile_link_color'] + ';'
+            profile_link_color = 'color:' + ' #' + user['profile_link_color'] + ';'
 
             tweet_dict = {
             #User tweet variables.
-            'tweet_text': tweet['text'],
+            'tweet_text': self.replace_url_to_link(tweet['text']),
             'user_nick': user['name'],
             'user_location': user_location,
             'user_acc_name': '@' + user['screen_name'],
             'follow_count': user['followers_count'],
             'profile_link': 'https://twitter.com/' + user['screen_name'],
-            #'profile_follow_link': 'https://twitter.com/' + user['screen_name'] + '/following',
+            'profile_follow_link': 'https://twitter.com/' + user['screen_name'] + '/following',
             #User image variables.
             'background_color': user['profile_background_color'],
             'link_color': user['profile_link_color'],
@@ -77,6 +104,8 @@ class TweeterView(BrowserView):
             'profile_image': profile_bigger,
             'profile_banner': profile_banner_url,
             'profile_color': profile_color,
+            'profile_border_color': profile_border_color,
+            'profile_link_color': profile_link_color,
             }
             data_list.append(tweet_dict)
 
