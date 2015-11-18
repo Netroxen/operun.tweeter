94b5ed380e96c10e5c354897ce6550cafd2ae9e1
Jesse Stippel <SkyeStippel@Gmail.com>
2015-11-18 13:19:54 +0100

    added link and text structuring

--- src/operun/tweeter/browser/tweeter.py
+++ src/operun/tweeter/browser/tweeter.py
@@ -1,20 +1,24 @@
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.statusmessages.interfaces import IStatusMessage #Error messages.
+# Error messages.
+from Products.statusmessages.interfaces import IStatusMessage
 
-from twitter import * #Twitter API module.
-import re #File string handling.
+from twitter import *  # Twitter API module.
+import re  # File string handling.
 
 TOKEN_KEY = '3946834222-BbW0VMZ0T4RzTxkIIzcjigt5PnefIq8bRBu5IcE'
 TOKEN_KEY_SECRET = 'nPfoG90sn7lAfjdCIX5GKvy1jgt53EtlpCbDL6lD1BUgG'
 CONSUMER_KEY = 'AxyyP3DfR6bXb4h3PJ3at5pzJ'
 CONSUMER_KEY_SECRET = 'ILBuekpLusqOd8b51lFx5hagX4chua9NB1OYDf1tk7R6RZU7Oj'
 
+
 class TweeterView(BrowserView):
 
     template = ViewPageTemplateFile('templates/tweeter.pt')
 
     def __call__(self):
+        self.tweet_loader = self.tweet_data()
+
         return self.template()
 
     def twitter_user(self):
@@ -24,18 +28,19 @@ class TweeterView(BrowserView):
         return this_result
 
     def tweet_count(self):
-        #Get number of tweets from formula and return as integer.
+        # Get number of tweets from formula and return as integer.
         form = self.request.form
         this_result = int(form.get('tweet_count', '6'))
 
         return this_result
 
-    def plone_message(self, msg, kind):
+    def spawn_message(self, msg, kind):
         message = IStatusMessage(self.request)
         message.add(msg, type=kind)
 
     def twitter_tweets(self):
-        t = Twitter(auth=OAuth(TOKEN_KEY, TOKEN_KEY_SECRET, CONSUMER_KEY, CONSUMER_KEY_SECRET))
+        t = Twitter(auth=OAuth(TOKEN_KEY, TOKEN_KEY_SECRET,
+                               CONSUMER_KEY, CONSUMER_KEY_SECRET))
         name = self.twitter_user()
 
         if name:
@@ -44,61 +49,65 @@ class TweeterView(BrowserView):
                 status_tweets = all_tweets['statuses']
                 tweet_list = status_tweets[:self.tweet_count()]
 
+                if len(status_tweets) < self.tweet_count():
+                    self.spawn_message(
+                        'Search returned less than ' + str(self.tweet_count()) + ' tweets.', 'info')
+
+                if len(status_tweets) == 0:
+                    self.spawn_message(
+                        'Search returned no results.', 'warning')
+
                 return tweet_list
             if '@' in name:
-                all_tweets = t.statuses.user_timeline(screen_name='%s' % (name))
+                all_tweets = t.statuses.user_timeline(
+                    screen_name='%s' % (name))
                 tweet_list = all_tweets[:self.tweet_count()]
+
+                if len(all_tweets) < self.tweet_count():
+                    self.spawn_message(
+                        'User has less than ' + str(self.tweet_count()) + ' tweets or doesn\'t exist.', 'info')
 
                 return tweet_list
             else:
-                all_tweets = t.statuses.user_timeline(screen_name='%s' % (name))
+                all_tweets = t.statuses.user_timeline(
+                    screen_name='%s' % (name))
                 tweet_list = all_tweets[:self.tweet_count()]
+
+                if len(all_tweets) < self.tweet_count():
+                    self.spawn_message(
+                        'User has less than ' + str(self.tweet_count()) + ' tweets or doesn\'t exist.', 'info')
 
                 return tweet_list
         else:
             return []
 
-    def tweet_info_message(self):
-        if self.tweet_count() == 6:
-            try:
-                all_tweets[6]
-            except IndexError:
-                self.plone_message('This user has less than 6 tweets.', 'info')
-        if self.tweet_count() == 12:
-            try:
-                all_tweets[12
-            except IndexError:
-                self.plone_message('This user has less than 12 tweets.', 'info')
-        if self.tweet_count() == 18:
-            try:
-                all_tweets[18]
-            except IndexError:
-                self.plone_message('This user has less than 18 tweets.', 'info')
-
     def replace_url_to_link(self, value):
-        #URL method.
-        urls = re.compile(r"((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)", re.MULTILINE|re.UNICODE)
+        # URL method.
+        urls = re.compile(
+            r"((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)", re.MULTILINE | re.UNICODE)
         value = urls.sub(r'<a href="\1">\1</a>', value)
 
-        #E-Mail method.
-        urls = re.compile(r"([\w\-\.]+@(\w[\w\-]+\.)+[\w\-]+)", re.MULTILINE|re.UNICODE)
+        # E-Mail method.
+        urls = re.compile(
+            r"([\w\-\.]+@(\w[\w\-]+\.)+[\w\-]+)", re.MULTILINE | re.UNICODE)
         value = urls.sub(r'<a href="mailto:\1">\1</a>', value)
 
         return value
 
     def replace_hash_word_col(self, value):
-        value = re.findall(r'#\w*', value)
+        hash_word = re.compile(r"([#@]\w+)", re.MULTILINE | re.UNICODE)
+        value = hash_word.sub(r'<span style="color: #2196f3">\1</span>', value)
 
         return value
 
     def tweet_data(self):
-        #Useable info from tweet data.
+        # Useable info from tweet data.
         data_list = []
 
         for tweet in self.twitter_tweets():
             user = tweet['user']
 
-            #Error handling.
+            # Error handling.
             if 'profile_banner_url' in user:
                 profile_banner_url = user['profile_banner_url']
             else:
@@ -109,30 +118,33 @@ class TweeterView(BrowserView):
             else:
                 user_location = ''
 
-            #Variables to pass through page template.
+            # Variables to pass through page template.
             profile_bigger = user['profile_image_url'].replace('_normal', '')
-            profile_color = 'background-color:' + ' #' + user['profile_link_color'] + ';'
-            profile_border_color = 'border: 1px solid #' + user['profile_link_color'] + ';'
-            profile_link_color = 'color:' + ' #' + user['profile_link_color'] + ';'
+            profile_color = 'background-color:' + \
+                ' #' + user['profile_link_color'] + ';'
+            profile_border_color = 'border: 1px solid #' + \
+                user['profile_link_color'] + ';'
+            profile_link_color = 'color:' + ' #' + \
+                user['profile_link_color'] + ';'
 
             tweet_dict = {
-            #User tweet variables.
-            'tweet_text': tweet['text'], #self.replace_url_to_link(tweet['text'])
-            'user_nick': user['name'],
-            'user_location': user_location,
-            'user_acc_name': '@' + user['screen_name'],
-            'follow_count': user['followers_count'],
-            'profile_link': 'https://twitter.com/' + user['screen_name'],
-            'profile_follow_link': 'https://twitter.com/' + user['screen_name'] + '/following',
-            #User image variables.
-            'background_color': user['profile_background_color'],
-            'link_color': user['profile_link_color'],
-            #User image link variables.
-            'profile_image': profile_bigger,
-            'profile_banner': profile_banner_url,
-            'profile_color': profile_color,
-            'profile_border_color': profile_border_color,
-            'profile_link_color': profile_link_color,
+                # User tweet variables.
+                'tweet_text': self.replace_hash_word_col(self.replace_url_to_link(tweet['text'])).replace('RT', ""),
+                'user_nick': user['name'],
+                'user_location': user_location,
+                'user_acc_name': '@' + user['screen_name'],
+                'follow_count': user['followers_count'],
+                'profile_link': 'https://twitter.com/' + user['screen_name'],
+                'profile_follow_link': 'https://twitter.com/' + user['screen_name'] + '/following',
+                # User image variables.
+                'background_color': user['profile_background_color'],
+                'link_color': user['profile_link_color'],
+                # User image link variables.
+                'profile_image': profile_bigger,
+                'profile_banner': profile_banner_url,
+                'profile_color': profile_color,
+                'profile_border_color': profile_border_color,
+                'profile_link_color': profile_link_color,
             }
             data_list.append(tweet_dict)
 
